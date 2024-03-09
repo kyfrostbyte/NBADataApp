791ad6a69f161ee5862eb522377a9f790b35e163
kyfrostbyte <94023660+kyfrostbyte@users.noreply.github.com>
2024-03-09 14:50:29 -0700

    Cleaned Code. Refactored async functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,9 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sport_scores/api/models/gamesbydate.dart';
 import 'api/api_service.dart';
-import 'package:http/http.dart' as http;
-import 'api/models/standingsby_year.dart';
-import 'api/models/standingsinfo.dart';
-import 'package:flutter/material.dart';
 import 'api/gameservice.dart';
 
-
-// void main() async {
-//   // Assuming fetchStandingsResult is the result from fetchStandings function
-//   FetchStandings? fetchStandingsResult = await fetchStandings();
-//
-//   // Check if fetchStandingsResult is not null and has a response
-//   if (fetchStandingsResult != null && fetchStandingsResult.response is List) {
-//     // Iterate through the list of teams
-//     List<Response> teamsList = fetchStandingsResult.response;
-//     for (var teamData in teamsList) {
-//       // Access and print information for each team
-//       Team team = teamData.team;
-//       print('Team Name: ${team.name}');
-//       print('Conference: ${teamData.conference.name}');
-//       print('Division: ${teamData.division.name}');
-//       print('Wins: ${teamData.win.total}');
-//       print('Losses: ${teamData.loss.total}');
-//       print('---------------------');
-//     }
-//   } else {
-//     print('Invalid response format or missing data');
-//   }
-// }
 
 void main() async {
   runApp(MyApp());
@@ -60,17 +32,6 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 
-  var favorites = <WordPair>[];
-
-  void toggleFavorite() {
-    if (favorites.contains(current)){
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-
   void getNext() {
     current = WordPair.random();
     notifyListeners();
@@ -92,10 +53,8 @@ class _MyHomePageState extends State<MyHomePage> {
         page = MyScoresPage();
         break;
       case 1:
-        page = GeneratorPage();
-        break;
-      case 2:
         page = Placeholder();
+        break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
@@ -111,15 +70,11 @@ class _MyHomePageState extends State<MyHomePage> {
                     destinations: [
                       NavigationRailDestination(
                         icon: Icon(Icons.home),
-                        label: Text('Home'),
+                        label: Text('Scores'),
                       ),
                       NavigationRailDestination(
                         icon: Icon(Icons.favorite),
-                        label: Text('Favorites'),
-                      ),
-                      NavigationRailDestination(
-                        icon: Icon(Icons.star),
-                        label: Text('Scores'),
+                        label: Text('Rankings'),
                       ),
                     ],
                     selectedIndex: selectedIndex,
@@ -145,134 +100,6 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of((context));
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",
-        ),
-      ),
-    );
-  }
-}
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty){
-      return Center(
-          child: Text('No Favorites Yet')
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have ' '${appState.favorites.length} favorites:'),
-        ),
-
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
-    );
-  }
-}
-
-
-
-
-class ScoresPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-    return ListView(
-      children: [
-        buildGameContainer('Washington Wizards', 'https://upload.wikimedia.org/wikipedia/fr/archive/d/d6/20161212034849%21Wizards2015.png', 102,
-                          'Toronto Raptors', 'https://upload.wikimedia.org/wikipedia/fr/8/89/Raptors2015.png', 107),
-        // Add more game containers as needed
-      ],
-    );
-  }
-
-  Widget buildGameContainer(String homeTeamName, String homeTeamLogoUrl, int homeScore, String visitorTeamName, String visitorTeamLogoUrl, int visitorScore) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-    );
-  }
-}
-
 class MyScoresPage extends StatefulWidget {
   const MyScoresPage({Key? key}) : super(key: key);
 
@@ -295,85 +122,74 @@ class _MyScoresPageState extends State<MyScoresPage> {
     return Scaffold(
         appBar: AppBar(title: const Text('People')),
         body: Center(
-          child: FutureBuilder<List<GameInfo>>(
-              future: futureGames,
-              builder: (context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.separated(
-                      itemBuilder: (context, index) {
-                        GameInfo game = snapshot.data?[index];
-                        return ListTile(
-                          title: Container(
-                            child: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Image.network(
-                                      game.homeLogo,
-                                      width: 30.0,
-                                      height: 30.0,
-                                    ),
-                                    Text(
-                                      game.homeTeamName,
-                                      style: TextStyle(fontWeight: FontWeight.bold),
-                                    ),
-                                    Text(
-                                      'Score: ${game.homeScore.toString()} ',
-                                      style: TextStyle(color: Colors.black),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(height: 20,),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Image.network(
-                                      game.visitorLogo,
-                                      width: 30.0,
-                                      height: 30.0,
-                                    ),
-                                    Text(
-                                      game.visitorTeamName,
-                                      style: TextStyle(fontWeight: FontWeight.bold),
-                                    ),
-                                    Text(
-                                      'Score: ${game.visitorScore.toString()}',
-                                      style: TextStyle(color: Colors.black),
-                                    ),
-                                  ],
-                                ),
-                              ],
+            child: FutureBuilder<List<GameInfo>>(
+                future: futureGames,
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.separated(
+                        itemBuilder: (context, index) {
+                          GameInfo game = snapshot.data?[index];
+                          return ListTile(
+                            title: Container(
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Image.network(
+                                        game.homeLogo,
+                                        width: 30.0,
+                                        height: 30.0,
+                                      ),
+                                      Text(
+                                        game.homeTeamName,
+                                        style: TextStyle(fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        'Score: ${game.homeScore.toString()} ',
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 20,),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Image.network(
+                                        game.visitorLogo,
+                                        width: 30.0,
+                                        height: 30.0,
+                                      ),
+                                      Text(
+                                        game.visitorTeamName,
+                                        style: TextStyle(fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        'Score: ${game.visitorScore.toString()}',
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                      separatorBuilder: ((context, index) {
-                        return const Divider(color: Colors.black);
-                      }),
-                      itemCount: snapshot.data!.length);
+                          );
+                        },
+                        separatorBuilder: ((context, index) {
+                          return const Divider(color: Colors.black);
+                        }),
+                        itemCount: snapshot.data!.length);
 
-                } else if (snapshot.hasError) {
-                  return(Text('ERROR: ${snapshot.error}'));
+                  } else if (snapshot.hasError) {
+                    return(Text('ERROR: ${snapshot.error}'));
+                  }
+
+                  return const CircularProgressIndicator();
                 }
-
-                return const CircularProgressIndicator();
-              }
-          )
+            )
         )
     );
   }
 }
 
 
-
-// ListView.builder(
-// itemCount: items.length,
-// itemBuilder: (context, index) {
-// final item = items[index];
-// return ListTile(
-// title: Text('Item $item'),
-// trailing: const Icon(Icons.chevron_right_outlined),
-// );
-// }
-// )
